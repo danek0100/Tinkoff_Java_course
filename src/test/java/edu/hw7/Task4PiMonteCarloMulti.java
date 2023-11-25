82249242e497965c1ae490ab143aeef5866740eb
danek0100 <71225718+danek0100@users.noreply.github.com>
2023-11-25 16:49:32 +0300

    HW-7 fix: multi-thread performance test deleted

--- src/test/java/edu/hw7/Task4PiMonteCarloMulti.java
+++ src/test/java/edu/hw7/Task4PiMonteCarloMulti.java
@@ -25,30 +25,4 @@ class Task4PiMonteCarloMultiTest {
         assertTrue(Math.abs(piEstimate - Math.PI) < errorMargin, "Оценка числа Пи должна быть в пределах заданной погрешности при использовании нескольких потоков");
     }
 
-    @Test
-    void testMultiThreadedPerformanceVsSingleThreaded() throws Exception {
-        int totalPoints = 10_000_000;
-        int numThreads = Runtime.getRuntime().availableProcessors();
-        if (numThreads > 1) {
-            double durationSingle = 0.0;
-            for (int i = 0; i < 10; ++i) {
-                long startTimeSingle = System.nanoTime();
-                Task4PiMonteCarloMulti.computePi(totalPoints, 1);
-                long endTimeSingle = System.nanoTime();
-                durationSingle += (endTimeSingle - startTimeSingle) / 1e9;
-            }
-            durationSingle /= 10;
-
-            double durationMulti = 0.0;
-            for (int i = 0; i < 10; ++i) {
-                long startTimeMulti = System.nanoTime();
-                Task4PiMonteCarloMulti.computePi(totalPoints, numThreads);
-                long endTimeMulti = System.nanoTime();
-                durationMulti += (endTimeMulti - startTimeMulti) / 1e9;
-            }
-            durationMulti /= 10;
-
-            assertTrue(durationMulti < durationSingle, "Многопоточная версия должна работать быстрее однопоточной");
-        }
-    }
 }
